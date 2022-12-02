d0e230cf7bfc59df7d3b41098bac25b004155680
devg1120 <devg1120@gmail.com>
2022-12-02 19:03:52 +0900

    update

---
+++ _common/_create_pattern/singleton/singleton_test.go
@@ -0,0 +1,21 @@
+package singleton
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestSingleton(t *testing.T)  {
+
+    for i := 0; i < 30; i++ {
+        go getInstance()
+    }
+
+    // Scanln is similar to Scan, but stops scanning at a newline and
+    // after the final item there must be a newline or EOF.
+    fmt.Scanln()
+
+}
+
+
+
---
+++ _common/_struct_pattern/flyweight/flyweight_test.go
@@ -0,0 +1,31 @@
+package flyweight
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestFlyweight(t *testing.T)  {
+
+    game := newGame()
+
+    //Add Terrorist
+    game.addTerrorist(TerroristDressType)
+    game.addTerrorist(TerroristDressType)
+    game.addTerrorist(TerroristDressType)
+    game.addTerrorist(TerroristDressType)
+
+    //Add CounterTerrorist
+    game.addCounterTerrorist(CounterTerrroristDressType)
+    game.addCounterTerrorist(CounterTerrroristDressType)
+    game.addCounterTerrorist(CounterTerrroristDressType)
+
+    dressFactoryInstance := getDressFactorySingleInstance()
+
+    for dressType, dress := range dressFactoryInstance.dressMap {
+        fmt.Printf("DressColorType: %s\nDressColor: %s\n", dressType, dress.getColor())
+    }
+
+}
+
+
